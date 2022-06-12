c01397ffca41a93d8664033613c8b77ff9f9f337
Gyanendra <sourcecodist@gmail.com>
2022-06-12 20:32:59 +0530

    Changes in server.py

--- src/server.py
+++ src/server.py
@@ -1,10 +1,10 @@
-# Server
 from flask import Flask
+
 server = Flask(__name__)
 
-@server.route('/')
-    def hello():
-        return "Hello world!"
+@server.route("/")
+def hello():
+    return "Hello World!"
 
-if __name__ = __main__
-    server.run(host='0.0.0.0')
+if __name__ == "__main__":
+   server.run(host='0.0.0.0')
